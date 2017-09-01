5b6758e6b8add4e8d2a9cc8f7257f4a1baf4f755
Matt Wynne <matthewctwynne@gmail.com>
2017-09-01 11:55:51 -0700

    Update for bonded interfaces.

--- site/openstack/manifests/profile/common/interfaces.pp
+++ site/openstack/manifests/profile/common/interfaces.pp
@@ -1,5 +1,5 @@
 class openstack::profile::common::interfaces {
-  $mgmt_interface = 'ens3'
+  $mgmt_interface = 'bond0'
   $vxlan_interface = 'ens4'
   $controller_mgmt_ip = '192.168.80.111'
   $mgmt_ip = $facts['networking']['interfaces'][$mgmt_interface]['ip']
