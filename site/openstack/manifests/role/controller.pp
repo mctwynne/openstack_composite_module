97b96824060a34181c202c717308063421b6e1bb
Matt Wynne <matthewctwynne@gmail.com>
2017-11-06 19:14:11 -0800

    Chain everything

--- site/openstack/manifests/role/controller.pp
+++ site/openstack/manifests/role/controller.pp
@@ -1,12 +1,12 @@
 class openstack::role::controller inherits ::openstack::role {
 
   class {'openstack::profile::rabbitmq': }
-  class {'::openstack::profile::mysql': }
+  -> class {'::openstack::profile::mysql': }
   -> class {'::openstack::profile::keystone': }
-  class {'::openstack::profile::glance': }
+  -> class {'::openstack::profile::glance': }
   -> class {'::openstack::profile::nova::api': }
   -> class {'::openstack::profile::neutron::api': }
   -> class {'::openstack::profile::neutron::control_agent': }
-  class {'::openstack::profile::horizon': }
-  class {'::openstack::profile::heat': }
+  -> class {'::openstack::profile::horizon': }
+  -> class {'::openstack::profile::heat': }
 }
