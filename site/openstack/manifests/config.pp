3bee13f1b0b8ed30d73a12fb465a957eda92abc3
Matt Wynne <matthewctwynne@gmail.com>
2017-12-06 17:36:58 -0800

    Fixed assignment operator.

--- site/openstack/manifests/config.pp
+++ site/openstack/manifests/config.pp
@@ -17,12 +17,12 @@ class openstack::config {
   $firewall_driver = 'iptables_hybrid'
   $ovs_tun_types = ['vxlan', 'gre', 'geneve']
   $global_physnet_mtu = '1450'
-  $type_drivers         = ['vxlan', 'gre', 'geneve', 'vlan', 'flat']
+  $type_drivers = ['vxlan', 'gre', 'geneve', 'vlan', 'flat']
   $tenant_network_types = ['vxlan', 'gre', 'geneve', 'vlan']
-  $vni_ranges           = ['100:2000']
-  $tunnel_id_ranges     = ['100:2000']
-  $bridge_uplinks   => ['br-ex:ens5']
-  $bridge_mappings  => ['external:br-ex']
+  $vni_ranges = ['100:2000']
+  $tunnel_id_ranges = ['100:2000']
+  $bridge_uplinks = ['br-ex:ens5']
+  $bridge_mappings = ['external:br-ex']
   $l3_interface_driver = 'openvswitch'
 
   if $router_distributed {
