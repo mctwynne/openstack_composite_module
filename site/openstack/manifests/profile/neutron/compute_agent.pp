f0f3f1d22c857016e77c0f5601c1d9dd175ce5bc
Matt Wynne <matthewctwynne@gmail.com>
2017-12-07 10:13:51 -0800

    Added conditional for DVR on compute nodes.

--- site/openstack/manifests/profile/neutron/compute_agent.pp
+++ site/openstack/manifests/profile/neutron/compute_agent.pp
@@ -17,17 +17,19 @@ class openstack::profile::neutron::compute_agent {
     firewall_driver  => $openstack::config::firewall_driver,
   }
 
-  class { '::neutron::agents::metadata':
-    debug                 => $openstack::config::debug,
-    metadata_insecure     => false,
-    shared_secret         => 'a_big_secret',
-    metadata_protocol     => $metadata_protocol,
-    metadata_ip           => $openstack::config::controller_mgmt_ip,
-  }
+  if $openstack::config::router_distributed {
+    class { '::neutron::agents::metadata':
+      debug                 => $openstack::config::debug,
+      metadata_insecure     => false,
+      shared_secret         => 'a_big_secret',
+      metadata_protocol     => $metadata_protocol,
+      metadata_ip           => $openstack::config::controller_mgmt_ip,
+    }
 
-  class { '::neutron::agents::l3':
-    interface_driver => $openstack::config::l3_interface_driver,
-    debug            => $openstack::config::debug,
-    agent_mode       => $openstack::config::compute_agent_mode,
+    class { '::neutron::agents::l3':
+      interface_driver => $openstack::config::l3_interface_driver,
+      debug            => $openstack::config::debug,
+      agent_mode       => $openstack::config::compute_agent_mode,
+    }
   }
 }
